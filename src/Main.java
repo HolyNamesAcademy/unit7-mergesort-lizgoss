369cbaff2bd9d952dd897cad25ac3c70e4275323
elizabeth <egossman18@holynames-sea.org>
2018-04-19 07:46:16 -0700

    hey

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
     /**
      * Puts the elements in the arrayList in sorted order from smallest to greatest.
      * This function uses selection sort to sort the arrayList.
+     *
      * @param arrayList the ArrayList to be sorted. arrayList cannot contain duplicates
      */
     public static void selectionSort(ArrayList<Integer> arrayList) {
@@ -20,8 +21,9 @@ public class Main {
     /**
      * Returns the index that value is located in the arrayList. This function searches linearly in
      * the arrayList to find the value.
+     *
      * @param arrayList the ArrayList containing the list of values to search. arrayList cannot contain duplicates
-     * @param value the value we are looking for in the array list
+     * @param value     the value we are looking for in the array list
      */
     public static int linearSearch(ArrayList<Integer> arrayList, int value) {
         throw new UnsupportedOperationException("LinearSearch() has not been implemented yet");
@@ -30,9 +32,10 @@ public class Main {
     /**
      * Returns the index that value is located in the arrayList. This function uses binary search in
      * the arrayList to find the value.
+     *
      * @param arrayList the ArrayList containing the list of values to search. THIS ARRAYLIST MUST BE
      *                  IN SORTED ORDER. arrayList cannot contain duplicates
-     * @param value the value we are looking for in the array list
+     * @param value     the value we are looking for in the array list
      */
     public static int binarySearch(ArrayList<Integer> arrayList, int value) {
         throw new UnsupportedOperationException("LinearSearch() has not been implemented yet");
@@ -41,17 +44,11 @@ public class Main {
     /**
      * Puts the elements in the arrayList in sorted order from smallest to greatest.
      * This function uses MergeSort to sort the arrayList.
+     *
      * @param arrayList the ArrayList to be sorted. arrayList cannot contain duplicates
      */
-    public static void mergeSort(ArrayList<Integer> arrayList)
-    {
-        //break up array list
-        if(arrayList.size() == 1)
-            return arrayList.get(1);
-        int size = arrayList.size();
-        arrayList<Integer> new1 = new ArrayList<Integer>(size/2);
-
-        //put back together
+    public static void mergeSort(ArrayList<Integer> arrayList) {
+        sort(arrayList, 0,arrayList.size());
     }
 
     /**
@@ -59,19 +56,19 @@ public class Main {
      * The function sorts the portion of arrayList specified by the range [lo, hi). The range
      * includes lo but excludes hi (arrayList[lo] is the first element in the range, but
      * arrayList[hi] is the first element after the last element in the range).
+     *
      * @param arrayList the ArrayList to be sorted.
-     * @param lo the index of the first element in the range
-     * @param hi the index of the last element in the range + 1.
+     * @param lo        the index of the first element in the range
+     * @param hi        the index of the last element in the range + 1.
      */
     public static void sort(ArrayList<Integer> arrayList, int lo, int hi) {
-        if (lo < hi)
-        {
+        if (lo < hi) {
             // Find the middle point
-            int m = (lo+hi)/2;
+            int m = (lo + hi) / 2;
 
             // Sort first and second halves
             sort(arrayList, lo, m);
-            sort(arrayList , m+1, hi);
+            sort(arrayList, m + 1, hi);
 
             // Merge the sorted halves
             merge(arrayList, lo, m, hi);
@@ -83,10 +80,11 @@ public class Main {
      * The function merges two consecutive, sorted ranges in the arrayList into one sorted range. The ranges
      * are specified as [lo, mid) and [mid, hi). Each range includes the first element, but excludes
      * the last element (the same way as in sort()).
+     *
      * @param arrayList the ArrayList to be sorted.
-     * @param lo the index of the first element in the first range
-     * @param mid the boundary point of the two ranges. arrayList[mid] is in the second range.
-     * @param hi the index of the last element in the second range + 1.
+     * @param lo        the index of the first element in the first range
+     * @param mid       the boundary point of the two ranges. arrayList[mid] is in the second range.
+     * @param hi        the index of the last element in the second range + 1.
      */
     public static void merge(ArrayList<Integer> arrayList, int lo, int mid, int hi) {
         ArrayList<Integer> sorted = new ArrayList<Integer>();
@@ -94,16 +92,24 @@ public class Main {
         int y = mid;
         int z = hi;
 
-        while (x < z/2 && y < z)
-        {
+        while (x < mid && y < hi) {
             if (arrayList.get(x) > arrayList.get(y)) {
-                sorted.add(y);
+                sorted.add(arrayList.get(y));
                 y += 1;
+            } else if (arrayList.get(x) < arrayList.get(y)) {
+                sorted.add(arrayList.get(x));
+                x += 1;
+            } else if (x == mid) {
+                sorted.add(arrayList.get(y));
+                y += 1;
+            } else if (y == hi) {
+                sorted.add(arrayList.get(x));
+                x += 1;
             }
-
-            if (arrayList.get(x) )
-
-
-
         }
+        for (int i = lo; i < hi - 2; i++)
+        {
+            arrayList.set(i, sorted.get(i));
+        }
+    }
 }
